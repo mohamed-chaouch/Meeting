658c9feea290b86c8c86541fab5af08dee3066e2
mohamed-chaouch <mohamedchaouch2212@gmail.com>
2024-12-16 14:21:47 +0100

    loader image

--- FrontEnd/components/Loader.tsx
+++ FrontEnd/components/Loader.tsx
@@ -5,14 +5,8 @@ const Loader = () => {
   return (
     <div className="flex items-center justify-center w-full h-screen">
       <div className="flex items-center justify-center">
-        <Image
-          src="/icons/logo.svg"
-          alt="loading circle"
-          width={70}
-          height={70}
-          className="animate-pulse"
-        />
-        <h1 className="text-2xl font-bold text-white">Meeting</h1>
+      <img src="/icons/logo.svg" alt="Meeting" className="m-0 w-11 h-11"/>
+        <h1 className="pl-2 text-2xl font-bold text-white">Meeting</h1>
       </div>
     </div>
   );
